91b14266a144788fd81ecc713e22c1f3c9a1e155
Ruser898 <amaury.alloyer@gmail.com>
2024-08-21 14:52:37 +0200

    Update streamlit_app_v2.py

--- streamlit_app_v2.py
+++ streamlit_app_v2.py
@@ -9,17 +9,7 @@ import statsmodels.api as sm
 from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
 import streamlit as st
-from st_files_connection import FilesConnection
 
-
-# Create connection object and retrieve file contents.
-# Specify input format is a csv and to cache the result for 600 seconds.
-conn = st.connection('s3', type=FilesConnection)
-df = conn.read("cmip6-data-derating-jrieke/myfile.csv", input_format="csv", ttl=600)
-
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.Owner} has a :{row.Pet}:")
 
 # functions
 def CDFt_ds(GF, GH, SH):
